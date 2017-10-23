853b454c2fca51612a1ebd124ea583916e5c750f
RobinDumontChaponet <robin@dumontchapo.net>
2017-10-23 18:56:45 +0200

    Update

---
+++ tests/test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Transitive\Core;
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
