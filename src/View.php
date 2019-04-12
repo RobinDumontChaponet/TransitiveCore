7b1b4eaa6ff44245a651bb695d08fa3df718d26b
Robin Dumont-Chaponet <src@dumontchapo.net>
2019-04-12 17:33:21 +0200

    Update View interface

--- src/View.php
+++ src/View.php
@@ -10,6 +10,10 @@ interface View
 
     public function getContent(string $key = null): ViewResource;
 
+    public function hasContent(?string $contentType = null, ?string $contentKey = null): bool;
+
+    public function addContent($content, ?string $contentType = null, ?string $contentKey = null): void;
+
     public function getHead(): ViewResource;
 
     public function __toString();
